e6870e682e287b3e2cd8c903c64bb210be94455d
JauniusPinelis <JauniusPinelis@gmail.com>
2021-11-11 18:21:39 +0200

    Init

---
+++ HorseRacingBackend/IntegrationTests/Factories/CustomWebApplicationFactory.cs
@@ -0,0 +1,50 @@
+﻿using HorseRacingBackend;
+using HorseRacingBackend.DAL;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace IntegrationTests.Factories
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var service = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<AppDbContext>));
+
+                services.Remove(service);
+
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<AppDbContext>();
+
+                    db.Database.EnsureCreated();
+
+                    try
+                    {
+                        //Utilities.InitializeDbForTests(db);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+            });
+        }
+    }
+}

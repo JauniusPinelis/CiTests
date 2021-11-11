ff9bc7dc96acb38a05fb9571d4c1217929314aa6
JauniusPinelis <JauniusPinelis@gmail.com>
2021-11-11 19:15:52 +0200

    INtegrations tests removed

--- HorseRacingBackend/IntegrationTests/Factories/CustomWebApplicationFactory.cs
+++ HorseRacingBackend/IntegrationTests/Factories/CustomWebApplicationFactory.cs
@@ -1,6 +1,11 @@
 ﻿using HorseRacingBackend;
+using HorseRacingBackend.DAL;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
 
 namespace IntegrationTests.Factories
 {
@@ -8,38 +13,38 @@ namespace IntegrationTests.Factories
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            //builder.ConfigureServices(services =>
-            //{
-            //    var service = services.SingleOrDefault(
-            //        d => d.ServiceType ==
-            //            typeof(DbContextOptions<AppDbContext>));
+            builder.ConfigureServices(services =>
+            {
+                var service = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<AppDbContext>));
 
-            //    services.Remove(service);
+                services.Remove(service);
 
-            //    services.AddDbContext<AppDbContext>(options =>
-            //    {
-            //        options.UseInMemoryDatabase("InMemoryDbForTesting");
-            //    });
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
 
-            //    var sp = services.BuildServiceProvider();
+                var sp = services.BuildServiceProvider();
 
-            //    using (var scope = sp.CreateScope())
-            //    {
-            //        var scopedServices = scope.ServiceProvider;
-            //        var db = scopedServices.GetRequiredService<AppDbContext>();
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<AppDbContext>();
 
-            //        db.Database.EnsureCreated();
+                    db.Database.EnsureCreated();
 
-            //        try
-            //        {
-            //            //Utilities.InitializeDbForTests(db);
-            //        }
-            //        catch (Exception ex)
-            //        {
+                    try
+                    {
+                        //Utilities.InitializeDbForTests(db);
+                    }
+                    catch (Exception ex)
+                    {
 
-            //        }
-            //    }
-            //});
+                    }
+                }
+            });
         }
     }
 }
